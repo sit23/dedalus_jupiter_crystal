714779cf900786e290cd778cb4edb036dfc8a8ba
caturrell <charlie.turrell@hotmail.co.uk>
2023-04-29 16:57:16 +0100

    velocity profile commit

--- vortices/velocity_profile.py
+++ vortices/velocity_profile.py
@@ -11,36 +11,44 @@ b1 = 1
 b2 = 1.5
 b3 = 2
 
-# Radius of vortex (km)
-rm = 1000
+# Radius of vortices at each pole (km)
+rm_north = 1000
+rm_south = 1200
 
-# maximum velocity of vortex
+# maximum velocity of vortex (m/s)
 vm = 80
 
 
 # Set up profile
 #----------------------------------------------------------------------------------------------
 
-# range of radius
-r = np.arange(-0*rm, 10*rm)
+# range of radius for each pole
+r_north = np.arange(0, 6*rm_north)
+r_south = np.arange(-6*rm_north, 0)
+
 
 # Velocity profile
-v1 = vm * ( r / rm ) * np.exp( (1/b1) * ( 1 - (r/rm)**b1 ) )
+v1 = vm * ( r_north / rm_north ) * np.exp( (1/b1) * ( 1 - ( r_north/rm_north )**b1 ) )
+v2 = vm * ( r_north / rm_north ) * np.exp( (1/b2) * ( 1 - ( r_north/rm_north )**b2 ) )
+v3 = vm * ( r_north / rm_north ) * np.exp( (1/b3) * ( 1 - ( r_north/rm_north )**b3 ) )
 
-v2 = vm * ( r / rm ) * np.exp( (1/b2) * ( 1 - (r/rm)**b2 ) )
-
-v3 = vm * ( r / rm ) * np.exp( (1/b3) * ( 1 - (r/rm)**b3 ) )
+v1_south = vm * ( r_south / -rm_south ) * np.exp( (1/b1) * ( 1 - ( r_south/-rm_south )**b1 ) )
+v2_south = vm * ( r_south / -rm_south ) * np.exp( (1/b2) * ( 1 - ( r_south/-rm_south )**b2 ) )
+v3_south = vm * ( r_south / -rm_south ) * np.exp( (1/b3) * ( 1 - ( r_south/-rm_south )**b3 ) )
 
 
 # Create plot
 #----------------------------------------------------------------------------------------------
 
 plt.figure()
-plt.plot(r, v1, label=r'$b=1$')
-plt.plot(r, v2, label=r'$b=1.5$')
-plt.plot(r, v3, label=r'$b=2$')
+plt.plot(r_north, v1, color='#1f77b4', label=r'$b=1$')
+plt.plot(r_north, v2, color='#ff7f0e', label=r'$b=1.5$')
+plt.plot(r_north, v3, color='#2ca02c', label=r'$b=2$')
+plt.plot(r_south, v1_south, color='#1f77b4')
+plt.plot(r_south, v2_south, color='#ff7f0e')
+plt.plot(r_south, v3_south, color='#2ca02c')
 plt.legend()
-plt.xlabel(r'$r$')
-plt.ylabel(r'$v(r)$')
+plt.xlabel('Distance (km)')
+plt.ylabel('Velocity (m/s)')
 plt.title('Velocity profile for varying steepness')
 plt.show()
