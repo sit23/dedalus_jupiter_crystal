f7c11f48e130325818e0a58e6673221a9ba0b08e
caturrell <charlie.turrell@hotmail.co.uk>
2023-07-07 18:18:57 +0100

    vorticity xarray

---
+++ reproduce/shielding/vorticity_xarray.py
@@ -0,0 +1,35 @@
+"""
+
+python3 ./original_files/vorticity_xarray.py
+
+"""
+
+
+import xarray as xar
+import dedalus.public as d3
+import matplotlib.pyplot as plt
+
+# Load the individual dedalus snapshots into xarray DataAarrays using dedalus' inbuilt 'load_tasks_to_xarray'
+task_list = [d3.load_tasks_to_xarray(f"./reproduce/shielding/diffusion/nu1e5_Bu10b2p5/shielding_snapshots/snapshots_s{snap_num+1}.h5") for snap_num in range(10)]
+
+# Create a list of variable names that are stored in these files
+list_var_names = [key for key in task_list[0].keys()]
+
+dataset_list = []
+
+# for each variable name, extract the data arrays that contain that data, and then merge them into a single xarray dataset using xarray concat
+for var_name in list_var_names:
+
+    list_data_arrays = [task_list_val[var_name] for task_list_val in task_list ]
+
+    dataset_list.append(xar.concat(list_data_arrays, dim='t'))
+
+#Finally, we merge the list of datasets for each variable into a single xarray dataset that contains all variables and all times.
+dataset = xar.merge(dataset_list)
+
+# plt.figure()
+# dataset['pheight'][:,64,:].plot.contourf(levels=30, cmap='RdBu_r')
+# plt.title('Perturbation height field at x=0.0 as a function of y and time.')
+
+# Download dataset
+dataset.to_netcdf('./reproduce/shielding/diffusion/nu1e5.nc') 
