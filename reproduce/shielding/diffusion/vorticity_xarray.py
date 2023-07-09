b2c80562fa2b5aef6ef7c1b8617f4a2551043e17
caturrell <charlie.turrell@hotmail.co.uk>
2023-07-09 19:23:33 +0100

    commit2

--- reproduce/shielding/diffusion/vorticity_xarray.py
+++ reproduce/shielding/diffusion/vorticity_xarray.py
@@ -9,7 +9,7 @@ import xarray as xar
 import dedalus.public as d3
 
 # Load the individual dedalus snapshots into xarray DataAarrays using dedalus' inbuilt 'load_tasks_to_xarray'
-task_list = [d3.load_tasks_to_xarray(f"./reproduce/shielding/diffusion/nu1e5_Bu10b2p5/shielding_snapshots/snapshots_s{snap_num+1}.h5") for snap_num in range(10)]
+task_list = [d3.load_tasks_to_xarray(f"./reproduce/shielding/diffusion/experiments/nu0p1e5_Bu10b2p5/shielding_snapshots/snapshots_s{snap_num+1}.h5") for snap_num in range(120)]
 
 # Create a list of variable names that are stored in these files
 list_var_names = [key for key in task_list[0].keys()]
@@ -27,4 +27,4 @@ for var_name in list_var_names:
 dataset = xar.merge(dataset_list)
 
 # Download dataset
-dataset.to_netcdf('./reproduce/shielding/diffusion/ds_nu1e5.nc') 
+dataset.to_netcdf('./reproduce/shielding/diffusion/ds_nu0p1e5.nc') 
